f20dd234e08013492767ae675bbb36d916d2bfa6
Abejide Ayodele <abejideayodele@gmail.com>
2015-01-13 03:56:21 -0600

    Added new csv with districts.

--- generate_reps.rb
+++ generate_reps.rb
@@ -7,12 +7,24 @@ agent = Mechanize.new
 reps_page = agent.get REPS_URL
 senators_page = agent.get SENATORS_URL
 
+
+
 def nass_members(page, reps, id="housetext_newbody")
-  page.search("//td[@id='#{id}']").each { |x| reps << x.text.gsub(/(.*)\n.*\n.*Political Party:(.*)\n.*State: (.*)/, '\1, \2, \3').gsub(/\s{2,}/, " ").strip }
+  links = page.links.select { |l| l.attributes.attributes["href"].value =~ /portfolio\/profile\.php\?id=/ }
+  links.each do |l|
+    link = l.click.search("//*[@id='#{id}']").first
+    next if link.nil?
+    if id == "housetext_newbody"
+      infos = link.css(".newsenn")
+    else
+      infos = link.xpath("span")
+    end
+    reps << infos.map(&:text).map(&:strip).join(",")
+  end
   next_page_link = page.links.find { |l| l.text == 'Next' }
   return reps if next_page_link.nil?
   nass_members(next_page_link.click, reps, id)
 end
 
-File.open("reps.csv", "w") { |f| f.puts nass_members(reps_page, []) }
-File.open("senators.csv", "w") { |f| f.puts nass_members(senators_page, [], "senatetext_newsbody") }
+File.open("new_reps.csv", "w") { |f| f.puts nass_members(reps_page, []) }
+File.open("new_senators.csv", "w") { |f| f.puts nass_members(senators_page, [], "senatetext_newsbody_large") }
