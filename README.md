4ba73f0f815277bc83ebe4a0fd659605389bb2db
Abejide Ayodele <abejideayodele@gmail.com>
2015-01-12 20:18:40 -0600

    Scrap the data.

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # reps
 Scrape to get details of representatives
+#### Requirement: ruby >= 2.0
 
-We want to get for each member of Senate and House of Reps:
+Run:
+- gem install bundler
+- bundle install
+- bundle exec ruby generate_reps.rb
 
-  - Name
-  - State
-  - Senatorial district
+to regenerate the csvs
 
-The urls for the scraping are:
+Data is scrapped from:
 
 http://www.nassnig.org/nass2/Princ_officers.php?pageNum_rs_senatorsList=3&totalRows_rs_senatorsList=362&title_sur=Sen.
 
---
+++ generate_reps.rb
@@ -0,0 +1,18 @@
+require "mechanize"
+
+REPS_URL = "http://www.nassnig.org/nass2/Princ_officers.php"
+SENATORS_URL = "http://www.nassnig.org/nass/Princ_officers.php?title_sur=Sen."
+
+agent = Mechanize.new
+reps_page = agent.get REPS_URL
+senators_page = agent.get SENATORS_URL
+
+def nass_members(page, reps, id="housetext_newbody")
+  page.search("//td[@id='#{id}']").each { |x| reps << x.text.gsub(/(.*)\n.*\n.*Political Party:(.*)\n.*State: (\w+).*/, '\1, \2, \3') }
+  next_page_link = page.links.find { |l| l.text == 'Next' }
+  return reps if next_page_link.nil?
+  nass_members(next_page_link.click, reps, id)
+end
+
+File.open("reps.csv", "w") { |f| f.puts nass_members(reps_page, []) }
+File.open("senators.csv", "w") { |f| f.puts nass_members(senators_page, [], "senatetext_newsbody") }
