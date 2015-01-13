b13b104ca5a9adfa927455e0a80b2a810f7f3901
Abejide Ayodele <abejideayodele@gmail.com>
2015-01-12 20:41:48 -0600

    Fix broken multi-word states.

--- generate_reps.rb
+++ generate_reps.rb
@@ -8,7 +8,7 @@ reps_page = agent.get REPS_URL
 senators_page = agent.get SENATORS_URL
 
 def nass_members(page, reps, id="housetext_newbody")
-  page.search("//td[@id='#{id}']").each { |x| reps << x.text.gsub(/(.*)\n.*\n.*Political Party:(.*)\n.*State: (\w+).*/, '\1, \2, \3') }
+  page.search("//td[@id='#{id}']").each { |x| reps << x.text.gsub(/(.*)\n.*\n.*Political Party:(.*)\n.*State: (.*)/, '\1, \2, \3').gsub(/\s{2,}/, " ").strip }
   next_page_link = page.links.find { |l| l.text == 'Next' }
   return reps if next_page_link.nil?
   nass_members(next_page_link.click, reps, id)
