1ab63d3c251e1f5b0a69ad9f5c19f0298ea1dd36
Abejide Ayodele <abejideayodele@gmail.com>
2015-01-13 20:25:22 -0600

    fix bad csv generation for reps.

--- generate_reps.rb
+++ generate_reps.rb
@@ -1,4 +1,5 @@
 require "mechanize"
+require "csv"
 
 REPS_URL = "http://www.nassnig.org/nass2/Princ_officers.php"
 SENATORS_URL = "http://www.nassnig.org/nass/Princ_officers.php?title_sur=Sen."
@@ -9,22 +10,26 @@ senators_page = agent.get SENATORS_URL
 
 
 
-def nass_members(page, reps, id="housetext_newbody")
+def nass_members(page, cursor, id="housetext_newbody")
   links = page.links.select { |l| l.attributes.attributes["href"].value =~ /portfolio\/profile\.php\?id=/ }
   links.each do |l|
     link = l.click.search("//*[@id='#{id}']").first
     next if link.nil?
     if id == "housetext_newbody"
       infos = link.css(".newsenn")
+      index = 1
     else
       infos = link.xpath("span")
+      index = 2
     end
-    reps << infos.map(&:text).map(&:strip).join(",")
+    row = infos.map(&:text).map(&:strip)
+    row[index] = row[index].scan(/(\w+[\w ]+),(.*)/)
+    cursor << row.flatten
   end
   next_page_link = page.links.find { |l| l.text == 'Next' }
-  return reps if next_page_link.nil?
-  nass_members(next_page_link.click, reps, id)
+  return cursor.close if next_page_link.nil?
+  nass_members(next_page_link.click, cursor, id)
 end
 
-File.open("new_reps.csv", "w") { |f| f.puts nass_members(reps_page, []) }
-File.open("new_senators.csv", "w") { |f| f.puts nass_members(senators_page, [], "senatetext_newsbody_large") }
+nass_members(reps_page, CSV.open("new_reps.csv", "w"))
+nass_members(senators_page, CSV.open("new_senators.csv", "w"), "senatetext_newsbody_large")
